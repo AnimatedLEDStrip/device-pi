a6cd05ce73a7257e525181bab4ff0ec837e02e28
Max Narvaez <narvae1@stolaf.edu>
2018-12-16 18:13:14 -0600

    Add hex property of ColorContainers Removes need to call getColorHex(). getColorHex() returns hex for backwards compatibility

--- src/ColorContainer.kt
+++ src/ColorContainer.kt
@@ -1,5 +1,12 @@
 open class ColorContainer(var r: Int, var g: Int, var b: Int) {
     constructor(hexIn: Long): this((hexIn and 0xFF0000 shr 16).toInt(), (hexIn and 0x00FF00 shr 8).toInt(), (hexIn and 0x0000FF).toInt())
+    var hex: Long
+        get() {
+            return (r shl 16).toLong() or (g shl 8).toLong() or b.toLong()
+        }
+        set(hexIn) {
+            setRGBFromHex(hexIn)
+        }
 
     fun setRGB(rIn: Int, gIn: Int, bIn: Int) {
         r = rIn
@@ -9,10 +16,6 @@ open class ColorContainer(var r: Int, var g: Int, var b: Int) {
 
     fun setRGBFromHex(hexIn: Long) {setRGB((hexIn and 0xFF0000 shr 16).toInt(), (hexIn and 0x00FF00 shr 8).toInt(), (hexIn and 0x0000FF).toInt())}
 
-    fun getColorHex(): Long {
-        var temp: Long = (r shl 16).toLong()
-        temp = temp or (g shl 8).toLong()
-        return (temp or b.toLong())
-    }
+    fun getColorHex() = hex
 
 }
