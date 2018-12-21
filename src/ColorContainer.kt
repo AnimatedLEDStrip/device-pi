dc631fa171e19c96a0d6d443681f6f6f089a3a91
Max Narvaez <narvae1@stolaf.edu>
2018-12-21 16:04:43 -0600

    Add toColor method in ColorContainer

--- src/ColorContainer.kt
+++ src/ColorContainer.kt
@@ -1,5 +1,8 @@
+import javafx.scene.paint.Color
+
 open class ColorContainer(var r: Int, var g: Int, var b: Int) {
-    constructor(hexIn: Long): this((hexIn and 0xFF0000 shr 16).toInt(), (hexIn and 0x00FF00 shr 8).toInt(), (hexIn and 0x0000FF).toInt())
+    constructor(hexIn: Long) : this((hexIn and 0xFF0000 shr 16).toInt(), (hexIn and 0x00FF00 shr 8).toInt(), (hexIn and 0x0000FF).toInt())
+
     var hex: Long
         get() {
             return (r shl 16).toLong() or (g shl 8).toLong() or b.toLong()
@@ -14,8 +17,12 @@ open class ColorContainer(var r: Int, var g: Int, var b: Int) {
         b = bIn
     }
 
-    fun setRGBFromHex(hexIn: Long) {setRGB((hexIn and 0xFF0000 shr 16).toInt(), (hexIn and 0x00FF00 shr 8).toInt(), (hexIn and 0x0000FF).toInt())}
+    fun setRGBFromHex(hexIn: Long) {
+        setRGB((hexIn and 0xFF0000 shr 16).toInt(), (hexIn and 0x00FF00 shr 8).toInt(), (hexIn and 0x0000FF).toInt())
+    }
 
     fun getColorHex() = hex
+
+    fun toColor() = Color.color((hex shr 16 and 0xFF) / 255.0, (hex shr 8 and 0xFF) / 255.0, (hex and 0xFF) / 255.0)
 
 }
