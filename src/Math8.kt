3cbc9945f08b5ea88955b10cfc6ac3f4263f39a6
Max Narvaez <narvae1@stolaf.edu>
2018-12-26 15:36:02 -0600

    Add maven framework

--- src/Math8.kt
+++
@@ -1,9 +0,0 @@
-fun blend8(a: Int, b: Int, amountOfB: Int): Int {
-    var partial: Int
-    val amountOfA = 255 - amountOfB
-    partial = a * amountOfA
-    partial += a
-    partial += (b * amountOfB)
-    partial += b
-    return partial shr 8
-}
--- src/scale8.kt
+++
@@ -1,1 +0,0 @@
-fun scale8(i: Int, scale: Int): Int = ((i * (scale + 1)) shr 8)
