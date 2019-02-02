ceea7693ee00a97e4a8e2f8cd3898416da6e05ba
Max Narvaez <narvae1@stolaf.edu>
2019-02-02 00:21:54 -0600

    Add documentation

--- src/main/java/animatedledstrip/leds/AnimatedLEDStripKotlinPi.kt
+++ src/main/java/animatedledstrip/leds/AnimatedLEDStripKotlinPi.kt
@@ -1,9 +1,16 @@
 package animatedledstrip.leds
 
+/**
+ * Class for running an LED strip from a Raspberry Pi.
+ *
+ * @param numLEDs Number of LEDs in the strip
+ * @param pin GPIO pin connected for signal
+ * @param imageDebugging Should a csv file be created containing all renders of
+ * the strip?
+ */
 class AnimatedLEDStripKotlinPi(numLEDs: Int,
                                pin: Int,
-                               emulated: Boolean = false,
-                               imageDebugging: Boolean = false): AnimatedLEDStrip(numLEDs, pin, emulated, imageDebugging) {
+                               imageDebugging: Boolean = false): AnimatedLEDStrip(numLEDs, pin, imageDebugging) {
 
     override var ledStrip: LEDStripInterface = WS281xCompat(pin, 255, numLEDs)
 }
--- src/main/java/animatedledstrip/leds/WS281xCompat.kt
+++ src/main/java/animatedledstrip/leds/WS281xCompat.kt
@@ -2,8 +2,15 @@ package animatedledstrip.leds
 
 import com.diozero.ws281xj.rpiws281x.WS281x
 
-class WS281xCompat(pin: Int, brightness: Int, numPixels: Int): WS281x(pin, brightness, numPixels), LEDStripInterface {
-    override val numLEDs: Int = numPixels
+
+/**
+ * Connection between the WS281x class and the LEDStripInterface interface.
+ *
+ * @param pin Pin the strip is connected to
+ * @param brightness Brightness of the strip
+ * @param numLEDs Number of LEDs in the strip
+ */
+class WS281xCompat(pin: Int, brightness: Int, override val numLEDs: Int): WS281x(pin, brightness, numLEDs), LEDStripInterface {
 
     override fun getPixelColor(pixel: Int): Int = getPixelColour(pixel)
     override fun setPixelColor(pixel: Int, color: Int) = setPixelColour(pixel, color)
