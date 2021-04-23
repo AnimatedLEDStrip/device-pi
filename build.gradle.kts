268955be6fdf54130fa272bb4f8d441a0ac31e5e
Max Narvaez <mnmax.narvaez3@gmail.com>
2021-04-22 20:46:24 -0500

    1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2018-2021 AnimatedLEDStrip
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 tasks.wrapper {
     gradleVersion = "6.7.1"
 }
@@ -29,27 +51,11 @@ kotlin {
     }
 }
 
-//sourceSets.main {
-//    dependencies {
-//        implementation("io.github.animatedledstrip:animatedledstrip-core-jvm:1.0.1")
-//        implementation("com.github.mbelling:rpi-ws281x-java:2.0.0-SNAPSHOT")
-//        implementation("org.apache.logging.log4j:log4j-core:2.13.2")
-//        implementation("org.apache.logging.log4j:log4j-api:2.13.2")
-//    }
-//
-//    java.srcDirs("src/main/kotlin")
-//}
-
 group = "io.github.animatedledstrip"
 version = "1.0.1"
 description = "A library for using the AnimatedLEDStrip library on Raspberry Pis"
 
-//java {
-//    withSourcesJar()
-//}
-
 val javadoc = tasks.named("javadoc")
-val jar = tasks.named("jar")
 
 val javadocJar by tasks.creating(Jar::class) {
     archiveClassifier.set("javadoc")
@@ -58,7 +64,6 @@ val javadocJar by tasks.creating(Jar::class) {
 
 publishing {
     publications.create<MavenPublication>("mavenPublication") {
-//        artifact(sun.tools.jar.resources.jar)
         artifact(tasks.jar.get())
         artifact(javadocJar)
         pom {
